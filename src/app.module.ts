eb3f6ef935d0c21ef7dc3e0c9d9d2c5cbadb5332
hienhayho <115549171+hienhayho@users.noreply.github.com>
2024-04-17 14:09:45 +0700

    complete auth module for login and register

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,12 @@
 import { MiddlewareConsumer, Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import { ConfigModule, ConfigService } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
 import { UserModule } from './v1/database/user/user.module';
 import { RoleModule } from './v1/database/role/role.module';
 import { AppLoggerMiddleware } from './middlware/request.logging';
+import { AuthModule } from './v1/auth/auth.module';
 
 @Module({
   imports: [
@@ -24,15 +25,16 @@ import { AppLoggerMiddleware } from './middlware/request.logging';
         synchronize: true,
       }),
     }),
+    AuthModule,
     UserModule,
     RoleModule,
   ],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule { 
+export class AppModule {
   configure(consumer: MiddlewareConsumer): void {
     consumer.apply(AppLoggerMiddleware).forRoutes('*');
   }
- }
+}
 
