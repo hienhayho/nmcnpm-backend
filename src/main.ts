ceb5b00c242918f875ea4558b79fdc3d327defee
hienhayho <115549171+hienhayho@users.noreply.github.com>
2024-04-23 08:28:52 +0700

    Revert "test"
    This reverts commit 9e48c36dc9a11d97708cd05b978bbe655bdf76bb.

--- src/main.ts
+++ src/main.ts
@@ -13,10 +13,6 @@ async function bootstrap() {
     .setTitle('NMCNPM BACKEND')
     .setDescription('Hotels Mangagement API')
     .setVersion('1.0')
-    .addBearerAuth(
-      { type: 'http', scheme: 'bearer', bearerFormat: 'JWT' },
-      'access-token',
-    )
     .build();
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('swagger', app, document);
