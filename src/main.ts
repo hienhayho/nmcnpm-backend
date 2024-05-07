96b375f94edc49d6a95358f9bd2902f1912a8d47
hienhayho <115549171+hienhayho@users.noreply.github.com>
2024-05-07 09:01:04 +0700

    add class validator and add to main.ts

--- src/main.ts
+++ src/main.ts
@@ -3,11 +3,13 @@ import { ConfigService } from '@nestjs/config';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import * as cookieParser from 'cookie-parser';
 import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors();
   app.use(cookieParser());
+  app.useGlobalPipes(new ValidationPipe());
   const configService = app.get(ConfigService);
   const config = new DocumentBuilder()
     .setTitle('NMCNPM BACKEND')
